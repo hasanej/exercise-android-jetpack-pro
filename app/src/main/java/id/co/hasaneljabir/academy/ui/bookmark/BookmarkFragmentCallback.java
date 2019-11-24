9443ad4221b939f0cfdf40c3858f5607d9576fd6
Hasan El Jabir <eljabirhasan@gmail.com>
2019-11-24 21:00:25 +0700

    Initial commit

---
+++ app/src/main/java/id/co/hasaneljabir/academy/ui/bookmark/BookmarkFragmentCallback.java
@@ -0,0 +1,7 @@
+package id.co.hasaneljabir.academy.ui.bookmark;
+
+import id.co.hasaneljabir.academy.data.CourseEntity;
+
+interface BookmarkFragmentCallback {
+    void onShareClick(CourseEntity course);
+}
